145fa9d80557936e15d93060d30f4e01e674981e
guoxiaolu <guoxiaolu06@126.com>
2018-03-05 14:35:32 +0800

    modify audio predict

--- extract_audio_feature.py
+++ extract_audio_feature.py
@@ -1,6 +1,5 @@
 import numpy as np
 import os
-from extractor import Extractor
 from tqdm import tqdm
 import glob
 from audio.vggish_inference import audio_inference
@@ -40,33 +39,26 @@ def extract_feature(video_path='data/video', audio_path='data/audio', sequence_p
         pbar.update(1)
     pbar.close()
 
-# def extract_one_feature(video, audio_path, sequence_path, seq_length=200, feature_length=128):
-    # model = Extractor()
-    # # model_resnet50 = Extractor(model_name='resnet50')
-    # img_list = glob.glob(os.path.join(audio_path, video + '_*.jpg'))
-    # if len(img_list) == 0:
-    #     return
-    #
-    # seqfile = os.path.join(sequence_path, video + '_' + str(seq_length) + '.npy')
-    # # Check if we already have it.
-    # if os.path.isfile(seqfile):
-    #     return
-    #
-    # img_list_sorted = sorted(img_list)
-    # frames = rescale_list(img_list_sorted, seq_length)
-    #
-    # sequence = []
-    # for image in frames:
-    #     if image != -1:
-    #         features = model.extract(image)
-    #         # features_resnet50 = model_resnet50.extract(image)
-    #         # features = np.concatenate((features, features_resnet50.flatten()))
-    #     else:
-    #         features = np.zeros((feature_length, 1))
-    #     sequence.append(features)
-    #
-    # # Save the sequence.
-    # np.save(seqfile, sequence)
+def extract_one_feature(video, audio_path, sequence_path, seq_length=200, feature_length=128):
+    wav_file = os.path.join(audio_path, video + '.wav')
+    if not os.path.isfile(wav_file):
+        return
+
+    seqfile = os.path.join(sequence_path, video + '_audio_' + str(seq_length) + '.npy')
+    # Check if we already have it.
+    if os.path.isfile(seqfile):
+        return
+
+    sequence = audio_inference(wav_file)
+    sequence_length = sequence.shape[0]
+    if sequence_length >= seq_length:
+        sequence = sequence[:seq_length, ]
+    else:
+        tmp = np.zeros((seq_length, feature_length), dtype=np.uint8)
+        tmp[:sequence_length, ] = sequence
+        sequence = tmp
+    # Save the sequence.
+    np.save(seqfile, sequence)
 
 def main():
     extract_feature()
--- predict.py
+++ predict.py
@@ -1,21 +1,31 @@
 import os
 import numpy as np
 from keras.models import load_model
-from extract_frame import extract_one_file
-from extract_feature import extract_one_feature
+import extract_frame
+import extract_feature
+import extract_audio
+import extract_audio_feature
 
-seq_length = 200
-video_name = 'data/video0/00001.mp4'
-model = load_model('model/weights.00200.hdf5')
+seq_length_frame = 400
+seq_length_audio = 200
+video_name = 'data/video0/00009.mp4'
+model = load_model('model/weights.00300.hdf5')
+model.summary()
 
 video_name_noext = video_name.split(os.path.sep)[-1].split('.')[0]
 save_path = './tmp/' + video_name_noext
 if not os.path.exists(save_path):
     os.makedirs(save_path)
 
-_, nb_frames = extract_one_file(video_name, save_path)
-extract_one_feature(video_name_noext, save_path, save_path)
-seqfile = os.path.join(save_path, video_name_noext + '_' + str(seq_length) + '.npy')
-sequence = np.load(seqfile)
-result = model.predict(np.expand_dims(sequence, axis=0))
+_, nb_frames = extract_frame.extract_one_file(video_name, save_path)
+extract_feature.extract_one_feature(video_name_noext, save_path, save_path)
+seqfile = os.path.join(save_path, video_name_noext + '_' + str(seq_length_frame) + '.npy')
+sequence_frame = np.load(seqfile)
+
+extract_audio.extract_one_file(video_name, save_path)
+extract_audio_feature.extract_one_feature(video_name_noext, save_path, save_path)
+seqfile = os.path.join(save_path, video_name_noext + '_audio_' + str(seq_length_audio) + '.npy')
+sequence_audio = np.load(seqfile)
+
+result = model.predict([np.expand_dims(sequence_frame, axis=0), np.expand_dims(sequence_audio, axis=0)])
 print result
